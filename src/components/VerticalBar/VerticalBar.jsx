c2d7fcba2ba604691840b2aa0e87d847a045d6ef
comrade19632 <comrade19632@gmail.com>
2020-04-08 13:30:39 +0300

    - Added graphs of relative values ​​and the ability to switch between graphs of relative values ​​and graphs of absolute values. - Added world to the dropdown list of the country selection.

--- src/components/VerticalBar/VerticalBar.jsx
+++ src/components/VerticalBar/VerticalBar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Bar} from "react-chartjs-2";
 
 
-
 class VerticalBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            countryName: 'Russia',
+            countryName: 'Весь мир',
+            dataType: 'Подтвержденные случаи'
         };
     }
 
@@ -17,7 +17,51 @@ class VerticalBar extends React.Component {
         let confirmedArrSelectedCountry = this.props.state.table[this.state.countryName].map(item => item["confirmed"]);
         let deathsArrSelectedCountry = this.props.state.table[this.state.countryName].map(item => item["deaths"]);
         let recoveredArrSelectedCountry = this.props.state.table[this.state.countryName].map(item => item["recovered"]);
+        let confirmedArrSelectedCountryDiff = [];
+        let deathsArrSelectedCountryDiff = [];
+        let recoveredArrSelectedCountryDiff = [];
+        for (let i = 1; i < confirmedArrSelectedCountry.length; i++) {
+            confirmedArrSelectedCountryDiff.push(confirmedArrSelectedCountry[i] - confirmedArrSelectedCountry[i - 1]);
+            recoveredArrSelectedCountryDiff.push(recoveredArrSelectedCountry[i] - recoveredArrSelectedCountry[i - 1]);
+            deathsArrSelectedCountryDiff.push(deathsArrSelectedCountry[i] - deathsArrSelectedCountry[i - 1]);
+        }
+        let dataType = {
+            'Подтвержденные случаи': {
+                datasets: [{
+                    label: `Подтвержденные случаи заражения(${this.state.countryName}) `,
+                    data: confirmedArrSelectedCountry,
+                    backgroundColor: 'rgba(0,102,204,1)'
+                }, {
+                    label: `Подтвержденные случаи смертей(${this.state.countryName}) `,
+                    data: deathsArrSelectedCountry,
+                    backgroundColor: 'rgba(255,0,0,1)'
+                }, {
+                    label: `Подтвержденные случаи выздоровления(${this.state.countryName}) `,
+                    data: recoveredArrSelectedCountry,
+                    backgroundColor: 'rgba(51,255,0,1)'
+                },
+                ],
+                labels: this.props.dateArr
+            },
+            'Прирост': {
+                datasets: [{
+                    label: `Прирост зараженных(${this.state.countryName}) `,
+                    data: confirmedArrSelectedCountryDiff,
+                    backgroundColor: 'rgba(0,102,204,1)'
+                }, {
+                    label: `Прирост смертей(${this.state.countryName}) `,
+                    data: deathsArrSelectedCountryDiff,
+                    backgroundColor: 'rgba(255,0,0,1)'
+                }, {
+                    label: `Прирост случаев выздоровления(${this.state.countryName}) `,
+                    data: recoveredArrSelectedCountryDiff,
+                    backgroundColor: 'rgba(51,255,0,1)'
+                },
+                ],
+                labels: this.props.dateArr.slice(1)
+            }
 
+        };
 
         return (
             <div className={'graph'}>
@@ -25,52 +69,16 @@ class VerticalBar extends React.Component {
                 <select name="countryName" onChange={this.props.handleChange.bind(this)}
                         value={this.state.countryName}>{this.props.countriesArr.map((item, index) => <option
                     key={index}>{item}</option>)} </select>
-
+                <select name='dataType' onChange={this.props.handleChange.bind(this)}
+                        value={this.state.dataType}>{['Подтвержденные случаи', 'Прирост'].map((item, index) => <option
+                    key={index}>{item}</option>)} </select>
                 <Bar data={{
-                    labels: this.props.dateArr,
-                    datasets: [{
-                        label: `Подтвержденные случаи заражения(${this.state.countryName}) `,
-                        data: confirmedArrSelectedCountry,
-                        backgroundColor: 'rgba(0,102,204,1)'
-                    }, {
-                        label: `Подтвержденные случаи смертей(${this.state.countryName}) `,
-                        data: deathsArrSelectedCountry,
-                        backgroundColor: 'rgba(255,0,0,1)'
-                    }, {
-                        label: `Подтвержденные случаи выздоровления(${this.state.countryName}) `,
-                        data: recoveredArrSelectedCountry,
-                        backgroundColor: 'rgba(51,255,0,1)'
-                    },
-
-
-                    ]
+                    labels: dataType[this.state.dataType].labels,
+                    datasets: dataType[this.state.dataType].datasets
                 }
                 }
                      width={100}
                      height={50}/>
-                <Bar data={{
-                    labels: this.props.dateArr,
-                    datasets: [{
-                        label: `Подтвержденные случаи заражения(Весь мир) `,
-                        data: this.props.confirmedArrWorld,
-                        backgroundColor: 'rgba(0,102,204,1)'
-                    }, {
-                        label: `Подтвержденные случаи смертей(Весь мир) `,
-                        data: this.props.deathsArrWorld,
-                        backgroundColor: 'rgba(255,0,0,1)'
-                    }, {
-                        label: `Подтвержденные случаи выздоровления(Весь мир) `,
-                        data: this.props.recoveredArrWorld,
-                        backgroundColor: 'rgba(51,255,0,1)'
-                    },
-
-
-                    ]
-                }
-                }
-                     width={100}
-                     height={50}/>
-
 
                 <a href="http://gradient-st.ru">Powered by Gradient Studio</a>
             </div>
